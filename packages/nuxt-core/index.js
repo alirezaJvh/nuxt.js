6bd73dc0d6a6ad02f63a55cd595fa369813b5979
Clark Du <clark.duxin@gmail.com>
2018-10-04 13:58:12 +0100

    fix: typo

--- packages/nuxt-core/index.js
+++ packages/nuxt-core/index.js
@@ -8,7 +8,7 @@
 const fs = require('fs')
 const path = require('path')
 
-if (fs.existsSync(path.resolve(__dirname, 'package.js1'))) {
+if (fs.existsSync(path.resolve(__dirname, 'package.js'))) {
   // Use esm version when using linked repository to prevent builds
   const requireModule = require('esm')(module)
   module.exports = requireModule('./src/index.js').default
